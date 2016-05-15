b514ac8903fc25b3ae067ed85bc89e5be7d8801d
don-durito <pitocco.ma@anche.no>
2016-05-16 00:53:20 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,13 +6,13 @@
 
 module ::WatchCategory
   def self.watch_category!
-    Referendum_category = Category.find_by_slug("Referendum")
-    Referendum_group = Group.find_by_name("Referendum")
-    return if announcements_category.nil? || Referendum_group.nil?
+    referendum_category = Category.find_by_slug("referendum")
+    referendum_group = Group.find_by_name("referendum")
+    return if referendum_category.nil? || referendum_group.nil?
 
-    Referendum_group.users.each do |user|
+    referendum_group.users.each do |user|
       watched_categories = CategoryUser.lookup(user, :watching).pluck(:category_id)
-      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], Referendum_category.id) unless watched_categories.include?(Referendum_category.id)
+      CategoryUser.set_notification_level_for_category(user, CategoryUser.notification_levels[:watching], referendum_category.id) unless watched_categories.include?(referendum_category.id)
     end
   end
 end
